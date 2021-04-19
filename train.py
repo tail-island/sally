cd1a5e4530527d3e32f434944e9bd281297b19f2
OJIMA Ryoji <rojima1@gmail.com>
2021-04-20 08:49:56 +0900

    refactored.

--- train.py
+++ train.py
@@ -14,18 +14,5 @@ model = SAC(
     seed=1234
 )
 
-# model = SAC(
-#     'MlpPolicy',
-#     env,
-#     learning_rate=0.0001,
-#     learning_starts=GAME_PERIOD_SEC * FPS * 10,
-#     gamma=0.999,
-#     use_sde=True,
-#     use_sde_at_warmup=True,
-#     verbose=1,
-#     policy_kwargs={'net_arch': [256, 256, 256]}
-#     seed=1234
-# )
-
 model.learn(total_timesteps=10_000_000, log_interval=10, callback=CheckpointCallback(save_freq=GAME_PERIOD_SEC * FPS * 10, save_path='log', name_prefix='self-driving'))
 model.save('self-driving')
